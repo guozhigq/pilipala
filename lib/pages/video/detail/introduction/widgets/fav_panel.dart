1ed7182481907fbb80d20edeaa5a0bf25a835b31
guozhigq <guozhigq@outlook.com>
2023-07-26 11:17:47 +0800

    fix: 收藏回退两次

--- lib/pages/video/detail/introduction/widgets/fav_panel.dart
+++ lib/pages/video/detail/introduction/widgets/fav_panel.dart
@@ -44,7 +44,6 @@ class _FavPanelState extends State<FavPanel> {
               TextButton(
                 onPressed: () async {
                   await widget.ctr!.actionFavVideo();
-                  Get.back();
                 },
                 child: const Text('完成'),
               ),
