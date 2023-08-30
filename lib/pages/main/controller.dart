fceb55aaa325d1a42e33462bb8712d4878d678a4
guozhigq <guozhigq@outlook.com>
2023-08-30 09:18:34 +0800

    feat: 两次退出确认

--- lib/pages/main/controller.dart
+++ lib/pages/main/controller.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
@@ -53,6 +54,7 @@ class MainController extends GetxController {
   final StreamController<bool> bottomBarStream =
       StreamController<bool>.broadcast();
   Box setting = GStrorage.setting;
+  DateTime? _lastPressedAt;
 
   @override
   void onInit() {
@@ -60,5 +62,17 @@ class MainController extends GetxController {
     if (setting.get(SettingBoxKey.autoUpdate, defaultValue: false)) {
       Utils.checkUpdata();
     }
+  }
+
+  Future<bool> onBackPressed(BuildContext context) {
+    if (_lastPressedAt == null ||
+        DateTime.now().difference(_lastPressedAt!) >
+            const Duration(seconds: 2)) {
+      // 两次点击时间间隔超过2秒，重新记录时间戳
+      _lastPressedAt = DateTime.now();
+      SmartDialog.showToast("再按一次退出Pili");
+      return Future.value(false); // 不退出应用
+    }
+    return Future.value(true); // 退出应用
   }
 }
