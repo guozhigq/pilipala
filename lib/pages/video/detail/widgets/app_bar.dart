6f84eefbe4beae269d4ff21c0b5d952239495023
guozhigq <guozhigq@outlook.com>
2023-09-04 11:16:52 +0800

    feat: 转发内容加上视频标题 issues#63

--- lib/pages/video/detail/widgets/app_bar.dart
+++ lib/pages/video/detail/widgets/app_bar.dart
@@ -48,15 +48,15 @@ class ScrollAppBar extends StatelessWidget {
                 ],
               ),
             ),
-            actions: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.share,
-                    size: 20,
-                  )),
-              const SizedBox(width: 12)
-            ],
+            // actions: [
+            //   IconButton(
+            //       onPressed: () {},
+            //       icon: const Icon(
+            //         Icons.share,
+            //         size: 20,
+            //       )),
+            //   const SizedBox(width: 12)
+            // ],
           ),
         ),
       ),
