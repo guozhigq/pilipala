844053b138ae7eba3cc07bbe0125e9b370b0001b
guozhigq <guozhigq@outlook.com>
2023-10-14 16:46:41 +0800

    mod: 首次登录时自动加载黑名单

--- lib/pages/home/controller.dart
+++ lib/pages/home/controller.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
+import 'package:pilipala/http/black.dart';
 import 'package:pilipala/models/common/tab_type.dart';
+import 'package:pilipala/models/user/black.dart';
 import 'package:pilipala/utils/storage.dart';
 
 class HomeController extends GetxController with GetTickerProviderStateMixin {
@@ -15,6 +17,13 @@ class HomeController extends GetxController with GetTickerProviderStateMixin {
   RxBool userLogin = false.obs;
   RxString userFace = ''.obs;
   var userInfo;
+  static Box setting = GStrorage.setting;
+  late List<int> blackMidsList;
+
+  int currentPage = 1;
+  int pageSize = 50;
+  int total = 0;
+  List<BlackListItem> blackList = [BlackListItem()];
 
   @override
   void onInit() {
@@ -51,7 +60,33 @@ class HomeController extends GetxController with GetTickerProviderStateMixin {
   void updateLoginStatus(val) async {
     userInfo = await userInfoCache.get('userInfoCache');
     userLogin.value = val ?? false;
-    if (val) return;
+    if (val) {
+      // 获取黑名单
+      await queryBlacklist();
+      blackMidsList = blackList.map<int>((e) => e.mid!).toList();
+      setting.put(SettingBoxKey.blackMidsList, blackMidsList);
+      return;
+    }
     userFace.value = userInfo != null ? userInfo.face : '';
+  }
+
+  Future queryBlacklist({type = 'init'}) async {
+    if (type == 'init') {
+      currentPage = 1;
+    }
+    var result = await BlackHttp.blackList(pn: currentPage, ps: pageSize);
+    if (result['status']) {
+      if (type == 'init') {
+        blackList = result['data'].list;
+        total = result['data'].total;
+      } else {
+        blackList.addAll(result['data'].list);
+      }
+      currentPage += 1;
+      if (blackList.length < total) {
+        await queryBlacklist(type: 'onLoad');
+      }
+    }
+    return result;
   }
 }
