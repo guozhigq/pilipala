b0c56feef5c6169eb088aa257005f2863009e3b8
guozhigq <guozhigq@outlook.com>
2024-02-07 02:47:11 +0800

    mod: 首页网络异常请求重试

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ import 'package:pilipala/utils/recommend_filter.dart';
 import 'package:catcher_2/catcher_2.dart';
 import './services/loggeer.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
@@ -55,14 +54,13 @@ void main() async {
       [FileHandler(await getLogsPath())],
     );
 
-    Catcher2(
-      debugConfig: debugConfig,
-      releaseConfig: releaseConfig,
-      runAppFunction: () {
-        runApp(const MyApp());
-      },
-    );
-
+    // Catcher2(
+    //   debugConfig: debugConfig,
+    //   releaseConfig: releaseConfig,
+    //   runAppFunction: () {
+    runApp(const MyApp());
+    //   },
+    // );
 
     // 小白条、导航栏沉浸
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
