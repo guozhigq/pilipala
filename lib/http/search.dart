92dfef2387162e001fa04d5b2f938f0dc4290580
guozhigq <guozhigq@outlook.com>
2024-05-01 20:44:38 +0800

    fix: 评论区跳转视频页pic null

--- lib/http/search.dart
+++ lib/http/search.dart
@@ -163,4 +163,20 @@ class SearchHttp {
       };
     }
   }
+
+  static Future<Map<String, dynamic>> ab2cWithPic(
+      {int? aid, String? bvid}) async {
+    Map<String, dynamic> data = {};
+    if (aid != null) {
+      data['aid'] = aid;
+    } else if (bvid != null) {
+      data['bvid'] = bvid;
+    }
+    final dynamic res =
+        await Request().get(Api.ab2c, data: <String, dynamic>{...data});
+    return {
+      'cid': res.data['data'].first['cid'],
+      'pic': res.data['data'].first['first_frame'],
+    };
+  }
 }
