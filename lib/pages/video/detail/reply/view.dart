a49429bbf168fb1c86f1d9d3e7b942ee8828e27f
guozhigq <guozhigq@outlook.com>
2024-10-31 23:05:57 +0800

    fix: replyAdd params

--- lib/pages/video/detail/reply/view.dart
+++ lib/pages/video/detail/reply/view.dart
@@ -312,13 +312,13 @@ class _VideoReplyPanelState extends State<VideoReplyPanel>
                               );
                             },
                           ).then(
-                            (value) => {
+                            (value) {
                               // 完成评论，数据添加
-                              if (value != null && value['data'] != null)
-                                {
-                                  _videoReplyController.replyList
-                                      .add(value['data'])
-                                }
+                              if (value != null && value['data'] != null) {
+                                _videoReplyController.replyList
+                                    .add(value['data']);
+                                _videoReplyController.replyList.refresh();
+                              }
                             },
                           );
                         },
