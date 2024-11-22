b824b75bc64f5b45a784965a99424282c3b69bc9
guozhigq <guozhigq@outlook.com>
2024-11-23 00:01:16 +0800

    opt: enableDlna switch callFn

--- lib/pages/setting/extra_setting.dart
+++ lib/pages/setting/extra_setting.dart
@@ -190,11 +190,14 @@ class _ExtraSettingState extends State<ExtraSetting> {
             setKey: SettingBoxKey.enableRelatedVideo,
             defaultVal: true,
           ),
-          const SetSwitchItem(
+          SetSwitchItem(
             title: '视频投屏开关',
             subTitle: '打开后将在播放器右上角显示投屏入口',
             setKey: SettingBoxKey.enableDlna,
             defaultVal: false,
+            callFn: (bool val) {
+              GlobalDataCache.enableDlna = val;
+            },
           ),
           ListTile(
             dense: false,
