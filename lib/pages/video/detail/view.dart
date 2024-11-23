e1948b159b0b6e129d993c1093f639d5a53cf7a4
guozhigq <guozhigq@outlook.com>
2024-11-24 01:16:33 +0800

    feat: comment can be closed

--- lib/pages/video/detail/view.dart
+++ lib/pages/video/detail/view.dart
@@ -24,6 +24,7 @@ import 'package:pilipala/pages/video/detail/related/index.dart';
 import 'package:pilipala/plugin/pl_player/index.dart';
 import 'package:pilipala/plugin/pl_player/models/play_repeat.dart';
 import 'package:pilipala/services/service_locator.dart';
+import 'package:pilipala/utils/global_data_cache.dart';
 import 'package:pilipala/utils/storage.dart';
 import 'package:status_bar_control/status_bar_control.dart';
 
@@ -779,13 +780,20 @@ class _VideoDetailPageState extends State<VideoDetailPage>
                             );
                           },
                         ),
-                        Obx(
-                          () => VideoReplyPanel(
-                            bvid: vdCtr.bvid,
-                            oid: vdCtr.oid.value,
-                            onControllerCreated: vdCtr.onControllerCreated,
-                          ),
-                        )
+                        if ((vdCtr.videoType == SearchType.media_bangumi &&
+                                GlobalDataCache.enableComment
+                                    .contains('bangumi')) ||
+                            (vdCtr.videoType == SearchType.video &&
+                                GlobalDataCache.enableComment
+                                    .contains('video'))) ...[
+                          Obx(
+                            () => VideoReplyPanel(
+                              bvid: vdCtr.bvid,
+                              oid: vdCtr.oid.value,
+                              onControllerCreated: vdCtr.onControllerCreated,
+                            ),
+                          )
+                        ],
                       ],
                     ),
                   ),
